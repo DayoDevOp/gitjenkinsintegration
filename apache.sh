808a82981231b5766f454d3a59342be2d259e139
Adedayo Adediran <adedayoadediran99@gmail.com>
2023-06-04 19:02:02 -0700

    httpd edit

--- apache.sh
+++ apache.sh
@@ -1,1 +1,9 @@
-testing
+#! /bin/bash
+
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "Welcome Home" > /var/www/html/index.html
+firewall-cmd --permanent --add-service={http,https}
+firewall-cmd --reload
+systemctl enable --now httpd
